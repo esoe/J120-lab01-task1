39e0dd1938929a8e22767b153b5ce25e74111eae
esoe <igor_nikulin@mail.ru>
2022-12-07 21:49:36 +0300

    перенес в отдельный репозитарий, чтоб проверять удобней было

---
+++ src/main/java/ru/molokoin/IPart.java
@@ -0,0 +1,30 @@
+package ru.molokoin;
+
+/**
+ * интерфейс IPart, прдусматривает основные методы,<p>
+ * для работы с секциями полного номера телефона
+ */
+public interface IPart {
+    public abstract int[] getValue();
+    public abstract void setValue(int[] value);
+    public abstract String toString();
+    public abstract void print();
+    /**
+     * метод убирает все не числовые поля в строке, <p>
+     * преобразует полученную строку в int[],<p>
+     * который возвращает в качестве результата выполнения метода.
+     * @param string
+     * @return
+     */
+    public static  int[] ofString(String string){
+        String s = string.replaceAll(Regex.NON_DIGIT.get(), "");
+        int[] result = new int[s.length()];
+        int i = 0;
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            result[i] = Character.getNumericValue(c);
+            i++;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/ru/molokoin/Number.java
@@ -0,0 +1,69 @@
+package ru.molokoin;
+
+import java.util.Arrays;
+
+/**
+ * Класс, хранящий данные о секции number полного номера телефона,
+ * реализует интерфейс IPart, прдусматривающий основные методы,<p>
+ * для работы с секциями полного номера телефона
+ */
+public class Number implements IPart{
+    /**
+     * Поле хранит набор цифр, составляющих секцию number полного номера телефона
+     */
+    private int[] value;
+
+    /**
+     * Основной конструктор класса, создает объекты<p>
+     * на основании строкового представления секции number полного номера телефона
+     * @param stringValue
+     */
+    Number(String stringValue){
+        setValue(IPart.ofString(stringValue));
+    }
+
+    @Override
+    public int[] getValue() {
+        return this.value;
+    }
+
+    @Override
+    public void setValue(int[] value) {
+        this.value = value;
+    }
+
+    /**
+     * выводит в консоль строковое представление секции number
+     */
+    @Override
+    public void print() {
+        System.out.println(toString());
+    }
+
+    /**
+     * Преобразование int[] в строку<p>
+     * с добавлением "-" перед последними двумя парами цифр<p>
+     * Примеры:<p>
+     * 000-00-00<p>
+     * 00-00-00
+     */
+    @Override
+    public String toString(){
+        String string = Arrays.toString(value).replaceAll(Regex.NON_DIGIT.get(), "");//получили строку цифр, без дефисов
+        int length = string.length();
+        String first = string.substring(0, length-4);
+        String second = string.substring(length-4, length-2);
+        String therd = string.substring(length-2, length);
+        String result = first + "-" + second + "-" + therd;
+        return result;
+    }
+    /**
+     * Метод создан, для проверки работоспособности класса
+     * @param args
+     */
+    public static void main(String[] args) {
+        Number number = new Number("123-34-45");
+        System.out.println(number.toString());
+        number.print();
+    }
+}
